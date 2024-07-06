e944d50525f6dc6dc6ad4fc73543b841e2e9335f
ChetanPrince <saini.rajvir2@gmail.com>
2024-07-06 11:15:03 +0200

    staged

--- script.js
+++ script.js
@@ -1,52 +1,41 @@
 
 const helloWorld = document.getElementById("hello-div");
-let msg = () =>{
-    if(element.classList.contains === success){
-        return (`<p>Success</p><p>The submission is successfully uploaded</p>`);
-    }
-    if(element.classList.contains === error){
-        return (`<p>Success</p><p>The submission encounters an eror.</p>`);
-    }
-    if(element.classList.contains === warning){
-        return (`<p>Success</p><p>The submission produces a warning.</p>`);
-    }
-    if(element.classList.contains === info){
-        return (`<p>Success</p><p>The submission requires additional information</p>`);
-    }
+
+function showToast(e){
+    e.preventDefault();
+    let toast = document.createElement("div");
+    toast.classList.add("toast");
+    let p1 = document.createElement("p");
+    p1.textContent = "Success";
+    toast.appendChild(p1);
+    let p2 = document.createElement("p");
+    p2.textContent = "The stage is cleared";
+    toast.appendChild(p2);
+    document.body.appendChild(toast);
     
-};
-
-function showToast(msg){
-    let toast = document.createElement(div;)
 }
-
-
-// function success(msg){
-//     let outputDiv = document.createElement("div");
-//     outputDiv.classList.add("toast");
-//     outputDiv.innerHTML = `msg;
-
-//     helloWorld.innerHTML = `<div id="flag-success" class="toast-progress">${msg}</div>`; 
-// }
-// function error(){
-//     helloWorld.innerHTML = `<div id="flag-error" class="toast-progress">Error Message</div>`; 
-// }
-// function warning(){
-//     helloWorld.innerHTML = `<div id="flag-warning" class="toast-progress">Warning Message</div>`; 
-// }
-// function info(){
-//     helloWorld.innerHTML = `<div id="flag-info" class="toast-progress">Information Message</div>`; 
-// }
-
-
-
 const saveBtn = document.getElementById("success");
 const errorBtn = document.getElementById("error");
 const warningBtn = document.getElementById("warning");
 const infoBtn = document.getElementById("info");
 
-saveBtn.addEventListener("click", success(successMsg));
-errorBtn.addEventListener("click", error);
-warningBtn.addEventListener("click", warning);
-infoBtn.addEventListener("click", info);
+saveBtn.addEventListener("click", showToast);
+errorBtn.addEventListener("click", showToast);
+warningBtn.addEventListener("click", showToast);
+infoBtn.addEventListener("click", showToast);
 
+
+// let msg = () =>{
+//     if(element.classList.contains === success){
+//         return (`<p>Success</p><p>The submission is successfully uploaded</p>`);
+//     }
+//     if(element.classList.contains === error){
+//         return (`<p>Success</p><p>The submission encounters an eror.</p>`);
+//     }
+//     if(element.classList.contains === warning){
+//         return (`<p>Success</p><p>The submission produces a warning.</p>`);
+//     }
+//     if(element.classList.contains === info){
+//         return (`<p>Success</p><p>The submission requires additional information</p>`);
+//     }
+// };
